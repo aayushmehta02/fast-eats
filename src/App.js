43952b99e648a43636913dac6288adc7eee5c227
aayushmehta02 <83535280+aayushmehta02@users.noreply.github.com>
2023-12-23 02:16:50 +0530

    food delivery app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import './App.css';
-
+import Navbar from './components/Navbar';
+import Login from './screens/Login';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Navbar/>}></Route>
+          <Route exact path='/login' element={<Login/>}></Route>
+        </Routes>
+      
+      
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { React } from "react";
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div>
+    <nav className="sticky navbar bg-success">
+        <ul className="nav-links">
+            <ul className="nav-links">  
+                <li><Link to="#">Session</Link></li>
+                <li><Link to="#">Feel Good Music</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="#">Breathe Timer</Link></li>
+
+                <Link to=""><i className="fa-regular fa-user" style={{color: 'black'}}></i></Link>
+            </ul>
+        </ul>
+    </nav>  
+    <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  )
+}
